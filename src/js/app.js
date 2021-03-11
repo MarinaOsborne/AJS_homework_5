0036179ac7d289b1f4e9aacbb7ac9b5435e86194
Marina Osborne <65596875+MarinaOsborne@users.noreply.github.com>
2021-03-11 16:57:47 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -5,11 +5,11 @@ const img = document.createElement('img');
 img.src = 'img/goblin.png';
 imgcell.appendChild(img);
 
-// setInterval(() => {
-//   const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-//   const filteredArr = arr.filter((item) => item !== random);
-//   const rand = filteredArr[Math.floor(Math.random() * 15)];
-//   const imgcellNew = document.getElementsByClassName('cell')[rand];
-//   imgcellNew.appendChild(img);
-//   random = rand;
-// }, 1000);
+setInterval(() => {
+  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const filteredArr = arr.filter((item) => item !== random);
+  const rand = filteredArr[Math.floor(Math.random() * 15)];
+  const imgcellNew = document.getElementsByClassName('cell')[rand];
+  imgcellNew.appendChild(img);
+  random = rand;
+}, 1000);
